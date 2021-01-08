572b2de2869daa94d01d6045641ebdb529018981
shockz-pulseHome <shockzinfinity@gmail.com>
2021-01-09 02:49:33 +0900

    init

---
+++ Extensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+  public static class Extensions
+  {
+    /// <summary>
+    /// Converts an object to its serialized XML format.
+    /// </summary>
+    /// <typeparam name="T">The type of object we are operating on</typeparam>
+    /// <param name="value">The object we are operating on</param>
+    /// <param name="removeDefaultXmlNamespaces">Whether or not to remove the default XML namespaces from the output</param>
+    /// <param name="omitXmlDeclaration">Whether or not to omit the XML declaration from the output</param>
+    /// <param name="encoding">The character encoding to use</param>
+    /// <returns>The XML string representation of the object</returns>
+    public static string ToXmlString<T>(this T value, bool removeDefaultXmlNamespaces = true, bool omitXmlDeclaration = true, Encoding encoding = null) where T : class
+    {
+      XmlSerializerNamespaces namespaces = removeDefaultXmlNamespaces ? new XmlSerializerNamespaces(new[] { XmlQualifiedName.Empty }) : null;
+
+      var settings = new XmlWriterSettings();
+      settings.Indent = true;
+      settings.OmitXmlDeclaration = omitXmlDeclaration;
+      settings.CheckCharacters = false;
+
+      using (var stream = new StringWriterWithEncoding(encoding))
+      using (var writer = XmlWriter.Create(stream, settings))
+      {
+        var serializer = new XmlSerializer(value.GetType());
+        serializer.Serialize(writer, value, namespaces);
+        return stream.ToString();
+      }
+    }
+
+    /// <summary>
+    /// Creates an object instance from the specified XML string.
+    /// </summary>
+    /// <typeparam name="T">The Type of the object we are operating on</typeparam>
+    /// <param name="value">The object we are operating on</param>
+    /// <param name="xml">The XML string to deserialize from</param>
+    /// <returns>An object instance</returns>
+    public static T FromXmlString<T>(this T value, string xml) where T : class
+    {
+      using (var reader = new StringReader(xml))
+      {
+        var serializer = new XmlSerializer(typeof(T));
+
+        return (T)serializer.Deserialize(reader);
+      }
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ConsoleApp1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //var todo = new Todo { Id = 1, Title = "Buy milk", UserId = 1 };
+      //string xml = todo.ToXmlString();
+      //File.WriteAllText("todo.xml", xml);
+
+      //string xml = File.ReadAllText("todo.xml");
+      //var todo = XmlHelper.DeserializeFromString<Todo>(xml);
+      string xml = File.ReadAllText("cancelList.xml");
+      var list = XmlHelper.DeserializeFromString<CancelResponseList>(xml);
+
+      string xml2 = list.ToXmlString(omitXmlDeclaration: false, encoding: Encoding.UTF8);
+      Console.WriteLine(xml2);
+
+      Console.WriteLine("Hello World!");
+      Console.ReadLine();
+    }
+  }
+
+  [XmlRoot("CancelResponseList")]
+  public class CancelResponseList : List<CancelResponse>
+  {
+  }
+
+  public class CancelResponse
+  {
+    public string RequestId { get; set; }
+    public string ResultCode { get; set; }
+    public string ResultMessage { get; set; }
+  }
+
+
+  public class Todo
+  {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int UserId { get; set; }
+  }
+}
